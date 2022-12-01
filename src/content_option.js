cc0d20a77e70d38a7a39930ab179311dcc26040e
TheOnis <onisamuelmichael@gmail.com>
2022-12-01 11:58:32 +0100

    moni portfolio

---
+++ src/content_option.js
@@ -0,0 +1,209 @@
+import imgone from '../src/assest/porta.png';
+import imgTwo from '../src/assest/port2.png';
+import imgThree from '../src/assest/port3.png';
+import imgFour from '../src/assest/port4.png';
+import imgFive from '../src/assest/port5.png';
+import imgSix from '../src/assest/port6.png';
+import imgSeven from '../src/assest/port7.png';
+import imgEight from '../src/assest/port8.png';
+import imgNine from '../src/assest/port9.png';
+import imgTen from '../src/assest/port10.png';
+import imgEleven from '../src/assest/port11.png';
+
+
+const logotext = "MICHAEL";
+const meta = {
+  title: "Michael Oni",
+  description:
+    "I’m Michael Oni _ Frontend Web devloper,currently working in Nigeria",
+};
+
+const introdata = {
+  title: "I’m Michael Oni",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop Responsive, Scalable Websites",
+  },
+  description:
+    "Frontend web developer and open-source enthusiast with good coding practices, Pair-programming and remote work. Fluent in several languages, frameworks, and technologies, capable of ramping up quickly and efficiently. A result-oriented Individual, astute with a solid Technical Operations background. Deep familiarity in ICT and digital forensics, with a passion for creating elegant and performant web applications. Fixed over 60 issues and Built over 5 web applications actively used in ZOJATECH Limited",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Frontend web developer and open-source enthusiast with good coding practices, Pair-programming and remote work. Fluent in several languages, frameworks, and technologies, capable of ramping up quickly and efficiently. A result-oriented Individual, astute with a solid Technical Operations background. Deep familiarity in ICT and digital forensics, with a passion for creating elegant and performant web applications. Fixed over 60 issues and Built over 5 web applications actively used in ZOJATECH Limited",
+};
+const worktimeline = [
+  {
+    jobtitle: "Project Lead Developer (Project Owner)",
+    where: "ZOJATECH LIMITED",
+    date: "2022",
+  },
+  {
+    jobtitle: "Frontend Developer",
+    where: "ZOJATECH LIMITED",
+    date: "2022",
+  },
+  {
+    jobtitle: "Frontend web Developer  (Intern)",
+    where: "OTHM SOFTWARE  TECHNOLOGY PARK LIMITED. (Local)",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Reactjs",
+    value: 90,
+  },
+  {
+    name: "Javascript",
+    value: 87,
+  },
+  {
+    name: "Python",
+    value: 40,
+  },
+  {
+    name: "HTML 5",
+    value: 95,
+  },
+  {
+    name: "CSS3",
+    value: 90,
+  },
+  {
+    name: "Bootstrap",
+    value: 85,
+  },
+  {
+    name: "Tailwind",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "Web Design",
+    description:
+      "I provide good user experience and helps your website visitors access and navigate your website with ease",
+  },
+  {
+    title: "Responsive Web Design",
+    description:
+      "Responsive web design (RWD) is a web development approach that creates dynamic changes to the appearance of a website, depending on the screen size and orientation of the device being used to view it",
+  },
+  {
+    title: "Scalable and Maintainable Website",
+    description:
+      "Scalable web architecture enables your web app to scale seamlessly with increased traffic. Compared to monolithic app design, scalable architecture can reduce costs while maintaining the required performance and functionality during cycles of growth",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: imgone,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgTwo,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgThree,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgFour,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgFive,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgSix,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: imgSeven,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgEight,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgNine,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgTen,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgEleven,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: imgSix,
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "onisamuelmichael@gmail.com",
+  YOUR_FONE: "(234) 7081575024",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+
+  YOUR_SERVICE_ID: "service_fltxe6e",
+  YOUR_TEMPLATE_ID: "contact_form",
+  YOUR_USER_ID: "L5-LpNhHWcD1GGY7x",
+};
+
+const socialprofils = {
+  github: "https://github.com/The0nis/",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/feed/",
+  twitter: "https://twitter.com/Oni__Michael",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
